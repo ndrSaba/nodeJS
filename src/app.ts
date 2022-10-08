9a20ee16b9888b8998761d887da986a7e14213b3
Andrea Sabato <102291579+ndrSaba@users.noreply.github.com>
2022-10-08 16:42:15 +0200

    es fatto

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import 'express-async-errors';
+
+const app = express();
+
+app.get('/planets', (request, response) => {
+    response.json([
+        {name: "Mercury"},
+        {name: "Venus"}
+    ]);
+});
+
+
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import 'dotenv/config';
+import app from './app';
+
+const port = process.env.PORT;
+
+app.listen(port, () =>{
+    console.log(`[server]: Server is running att https://localhost:${port}`);
+})
