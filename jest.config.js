45b413a4ea8da1c290f70b071c7bcd4803ccf232
Andrea Sabato <102291579+ndrSaba@users.noreply.github.com>
2022-10-10 14:24:34 +0200

    es fatto

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
     verbose: true,
-    setupFilesAfterEnv: ["./src/lib/prisma/client.mock.ts"],
+    setupFilesAfterEnv: [
+        "./src/lib/prisma/client.mock.ts",
+        "./src/lib/middleware/multer.mock.ts",
+    ],
 };
