8936d8400e87fc8ba0686b03bd999ae63d425cfb
Andrea Sabato <102291579+ndrSaba@users.noreply.github.com>
2022-10-10 14:27:23 +0200

    es fatto

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import 'dotenv/config';
+import config  from './config';
 import app from './app';
 
-const port = process.env.PORT;
+const port = config.PORT;
 
 app.listen(port, () =>{
     console.log(`[server]: Server is running att https://localhost:${port}`);
